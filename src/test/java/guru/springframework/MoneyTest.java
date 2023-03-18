0c901c4809c2d018cdc0023a4deaa73170d3d09d
Daniel Martínez <aprendejava73@gmail.com>
2023-03-18 09:56:46 +0100

    19. Equality for All

--- src/test/java/guru/springframework/MoneyTest.java
+++ src/test/java/guru/springframework/MoneyTest.java
@@ -3,8 +3,15 @@ package guru.springframework;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 public class MoneyTest {
+
+    @Test
+    void testEquality() {
+        assertEquals(new Dollar(5), new Dollar(5));
+        assertNotEquals(new Dollar(5), new Dollar(8));
+    }
 
     @Test
     void testMultiplication() {
