eeba6a92862ca0f9ea9e4b8e15aedf26b0a7513d
Daniel Martínez <aprendejava73@gmail.com>
2023-03-22 20:02:33 +0100

    24. Makin' Objects

--- src/main/java/guru/springframework/Franc.java
+++ src/main/java/guru/springframework/Franc.java
@@ -1,20 +1,12 @@
 package guru.springframework;
 
-public class Franc {
-    int amount;
+public class Franc extends Money {
 
     public Franc(int amount) {
         this.amount = amount;
     }
 
-    Franc times(int multiplier) {
+    public Money times(int multiplier) {
         return new Franc(amount * multiplier);
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        Franc franc = (Franc) obj;
-
-        return this.amount == franc.amount;
     }
 }
