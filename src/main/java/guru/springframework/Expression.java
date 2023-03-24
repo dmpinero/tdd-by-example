9ffb21ffa4ae83d2dc3bfeb117c71b3d65309ad8
Daniel Martínez <aprendejava73@gmail.com>
2023-03-24 17:56:46 +0100

    32. Abstraction, Finally

--- src/main/java/guru/springframework/Expression.java
+++ src/main/java/guru/springframework/Expression.java
@@ -3,4 +3,5 @@ package guru.springframework;
 public interface Expression {
     Money reduce(Bank bank, String to);
     public Expression plus(Expression addend);
+    Expression times(int multiplier);
 }
